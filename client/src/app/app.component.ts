420bc8f7790e885a0f6e0c8f64f5a4a313b2d84e
Maciuska <Maciuska14@gmail.com>
2017-03-09 21:45:25 +0000

    Basic things

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SellersService, Seller} from './sellers.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
+
+ // private sellers: Seller[];
+ private seller: Seller;
+
+  constructor(private service: SellersService) {}
+
+  ngOnInit() {
+
+    this.service.getSellerById(2).subscribe((result) => {
+      this.seller = result;
+    }, (err) => {
+      //TODO
+      console.log("There was a failure");
+    });
+
+    /*
+    this.service.getSellers().subscribe(result => {
+      this.sellers = result;
+    });
+    */
+  }
+
 }
+
+
