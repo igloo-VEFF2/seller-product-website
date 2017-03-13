6111744d5a7caa9aeea281590c981f9a824055b9
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-13 19:18:22 +0000

    adding code to display seller info

--- client/src/app/seller/seller.component.ts
+++ client/src/app/seller/seller.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SellersService, Seller, SellerDetails, Product } from '../sellers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-seller',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private service: SellersService) { }
+
+  private sellerID: number;
+  private name: string;
+  private category: string;
+  private imagePath: string;
+  private productList: Product[];
+  private sellerInfo: SellerDetails;
 
   ngOnInit() {
+    this.sellerID = this.route.snapshot.params['id'];
+    this.service.getSellerById(this.sellerID).subscribe(info => {
+      console.log("hello");
+    });
   }
 
 }
