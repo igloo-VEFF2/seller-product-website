53b71b230facbb60f41914f9e3e7ec779624d470
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-13 04:02:43 +0000

    added POST and PUT to services, need to be reviewed

--- client/src/app/sellers.service.ts
+++ client/src/app/sellers.service.ts
@@ -28,7 +28,7 @@ export class SellersService {
 
   constructor(private http: Http) { }
 
-  getSellers() : Observable<Seller[]> {
+  getSellers(): Observable<Seller[]> {
     return this.http.get('http://localhost:5000/api/sellers')
     .map(response => {
       return <Seller[]> response.json();
@@ -46,15 +46,44 @@ export class SellersService {
 
   getProductsBySellerId(id: number): Observable<Product[]> {
     return this.http.get(`http://localhost:5000/api/sellers/${id}/products`)
-    .map(response =>{
+    .map(response => {
       return <Product[]> response.json();
     });
     //TODO: add response for (ID) not found and server not up
   }
 
-  //TODO: finish functions below
-  //addNewSeller()
-  //addNewProduct(id: number)
-  //updateSellerDetails(id: number)
-  //updateProductDetails(sellerID: number, productID: number)
+  //TODO: check if these functions do the correct thing
+  //console.log checks added to see data
+  addNewSeller(newSeller: Seller): Observable<Seller> {
+    console.log(newSeller);
+    return this.http.post('http://localhost:5000/api/sellers', newSeller)
+    .map(response => {
+      console.log(response);
+      return <Seller> response.json(); //this is very probably wrong
+    });
+  }
+  
+  addNewProduct(id: number, newProduct: Product): Observable<Product> {
+    console.log(id);
+    console.log(newProduct);
+    return this.http.post(`http://localhost:5000/api/sellers/${id}/products`, newProduct)
+    .map(response => {
+      console.log(response);
+      return <Product> response.json();
+    });
+  }
+
+  updateSellerDetails(id: number, updatedSeller: Seller): Observable<Seller> {
+    return this.http.put(`http://localhost:5000/api/sellers/${id}`, updatedSeller)
+    .map(response => {
+      return <Seller> response.json();
+    });
+  }
+
+  updateProductDetails(sellerID: number, productID: number, updatedProduct: Product): Observable<Product> {
+    return this.http.put(`/api/sellers/${sellerID}/products/${productID}`, updatedProduct)
+    .map(response => {
+      return <Product> response.json();
+    });
+  }
 }
