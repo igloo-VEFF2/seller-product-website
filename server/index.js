f01e4a226df6b4751abfcfece6674437d754614b
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-14 02:08:34 +0000

    fixing errors in server file

--- server/index.js
+++ server/index.js
@@ -123,7 +123,10 @@ app.post("/api/sellers", (req, res) => {
 
 app.put("/api/sellers/:id", (req, res) => {
 	// Check if we can find the seller:
-	var seller = findSellerById(req.params.id);
+	var id = parseInt(req.params.id);
+
+	var seller = findSellerById(id);
+
 	if (!seller) {
 		res.statusCode = 404;
 		return res.send('Error 404: No seller found!');
