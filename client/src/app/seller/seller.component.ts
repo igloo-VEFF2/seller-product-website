2b060784e90cdb4970dcfc1b50ab60a2b546c0f2
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-14 00:59:41 +0000

    added code for edit seller, adding new product and editing product

--- client/src/app/seller/seller.component.ts
+++ client/src/app/seller/seller.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SellersService, Seller, Product } from '../sellers.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SellerDlgComponent } from '../seller-dlg/seller-dlg.component';
+import { ProductDlgComponent } from '../product-dlg/product-dlg.component';
 
 @Component({
   selector: 'app-seller',
@@ -11,7 +14,8 @@ export class SellerComponent implements OnInit {
 
   constructor(private router: Router,
     private route: ActivatedRoute,
-    private service: SellersService) { }
+    private service: SellersService,
+    private modalService: NgbModal) { }
 
   private sellerID: number;
   private name: string;
@@ -19,6 +23,7 @@ export class SellerComponent implements OnInit {
   private imagePath: string;
   private productList: Product[];
   private topTen: Product[];
+  private product: Product;
 
   ngOnInit() {
     this.sellerID = this.route.snapshot.params['id'];
@@ -41,10 +46,58 @@ export class SellerComponent implements OnInit {
   }
 
   editSellerInfo() {
-    console.log("editing seller");
+    console.log('editing seller');
+    let editSellerInstance = this.modalService.open(SellerDlgComponent);
+
+    editSellerInstance.componentInstance.seller = {
+      name: this.name,
+      category: this.category,
+      imagePath: this.imagePath
+    };
+
+    editSellerInstance.result.then(obj => {
+      console.log(obj);
+      this.service.updateSellerDetails(this.sellerID, obj);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
   }
 
   addNewProduct() {
-    console.log("adding new product");
+    console.log('adding new product');
+    let newProductInstance = this.modalService.open(ProductDlgComponent);
+
+    newProductInstance.componentInstance.product = { };
+
+    newProductInstance.result.then(obj => {
+      console.log(obj);
+      this.service.addNewProduct(this.sellerID, obj);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  editProduct() {
+    console.log('editing product');
+    let editProductInstance = this.modalService.open(ProductDlgComponent);
+
+    editProductInstance.componentInstance.product = {
+      name: this.product.name,
+      price: this.product.price,
+      quantitySold: this.product.quantitySold,
+      quantityInStock: this.product.quantityInStock,
+      imagePath: this.product.imagePath
+    }
+
+    editProductInstance.result.then(obj => {
+      console.log(obj);
+      this.service.updateProductDetails(this.sellerID, this.product.id, obj);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 }
