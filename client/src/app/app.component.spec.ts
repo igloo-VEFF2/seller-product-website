ba3739dfd04c8d4201eb5f686bd4e4ba89443b43
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-03-14 20:10:02 +0000

    added unit tests, complains about router-outlet

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  /*it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app works!');
@@ -30,5 +30,11 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  }));*/
+
+  it(`should have h1 tag with the text 'E-bai'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const title = fixture.debugElement.componentInstance;
+    expect(title.querySelector('h1').textContent).toContain('E-bai');
   }));
 });
